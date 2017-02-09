a85e9decd5b6920a4f59d70889c6eda4db3a907c
Steven Haye <Steven Haye>
2017-02-09 00:01:03 -0800

    First commit!

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var Drink;
+var Size;
+var Quantity;
+
+
+function price_calc(Size, Quantity) {
+  var price;
+  var tax = .08;
+  if (Size == 16){
+    price = 5;
+  }else if (Size == 20){
+    price = 6;
+  }else {
+    price = 4;
+  }
+  var totalPrice = (price * Quantity) + price * tax;
+  return totalPrice;
+}
+var inventory = {
+  "Lager" : 640,
+  "Pale Ale" : 640,
+  "IPA" : 640,
+  "Stout": 640
+}
